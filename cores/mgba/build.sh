52e457089d5a90f291928165aa7b74a07abe993b
Devon Bontrager <139504713+dbontr@users.noreply.github.com>
2026-09-06 08:31:47 -0500

    Keep mGBA memory fixed within the closed ABI

--- cores/mgba/build.sh
+++ cores/mgba/build.sh
@@ -129,8 +129,7 @@ emcc \
     -sSTANDALONE_WASM=1 \
     -sFILESYSTEM=0 \
     -sINITIAL_MEMORY=67108864 \
-    -sALLOW_MEMORY_GROWTH=1 \
-    -sMAXIMUM_MEMORY=134217728 \
+    -sALLOW_MEMORY_GROWTH=0 \
     -sSTACK_SIZE=1048576 \
     -sMALLOC=dlmalloc \
     -Wl,--allow-undefined-file="${ROOT}/scripts/wisp-core-imports.txt" \
